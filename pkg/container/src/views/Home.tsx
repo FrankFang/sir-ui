699c1fcbf78afb4d1bbae1a8a6db7d954ec469e4
frankfang <frankfang1990@gmail.com>
2022-10-21 17:43:16 +0800

    添加 sir-ui 内容

--- pkg/container/src/views/Home.tsx
+++ pkg/container/src/views/Home.tsx
@@ -9,13 +9,27 @@ export const Home = defineComponent({
         </nav>
         <main text-center flex flex-col gap-y-16px py-16px>
           <img src={logo} alt="logo" />
-          <h1 text-6xl>Demo</h1>
+          <h1 text-6xl>铯UI</h1>
           <div flex gap-x-16px justify-center align-center>
             <button btn btn-primary>开始使用</button>
             <button btn btn-secondary>查看文档</button>
             <button btn btn-secondary>切换主题</button>
           </div>
         </main>
+        <section flex flex-col gap-y-8px p-32px text-gray-500>
+          <h2 text-black mt-32px>支持多种框架</h2>
+          <div>
+            Vue 2、Vue 3、React 等主流框架均可使用
+          </div>
+          <h2 text-black mt-32px>代码工整</h2>
+          <div>
+            如果你想学习如何制作 UI 组件库，可以参考我们的源码
+          </div>
+          <h2 text-black mt-32px>功能全面</h2>
+          <div>
+            支持主题切换、Hooks API、TypeScript 等等
+          </div>
+        </section>
       </div>
     )
   },
